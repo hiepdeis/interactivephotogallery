1c84befaa31a1efb615dbbdc023dd0bdd67ae8c5
hiepdeis <trungthanh26148@gmail.com>
2026-07-01 14:14:44 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function upDate(previewPic){
+
+    console.log(previewPic.alt);
+
+    console.log(previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+    document.getElementById("image").style.backgroundImage =
+    "url('" + previewPic.src + "')";
+
+}
+
+function undo(){
+
+    document.getElementById("image").style.backgroundImage =
+    "url('')";
+
+    document.getElementById("image").innerHTML =
+    "Hover over an image below to display here.";
+
+}
